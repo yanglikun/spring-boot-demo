fb694d12f0c79c9b6112c486bb5179e9409ad5b4
likunyang <yanglikun0@gmail.com>
2017-03-16 07:24:44 +0800

    修改jar to war

--- src/main/java/com/example/SpringBootDemoApplication.java
+++ src/main/java/com/example/SpringBootDemoApplication.java
@@ -2,9 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SpringBootDemoApplication.class);
