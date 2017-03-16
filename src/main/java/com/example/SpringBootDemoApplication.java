a99e814850e37417d4fff731a2ae5b76f2c8369a
likunyang <yanglikun0@gmail.com>
2017-03-16 20:34:19 +0800

    删除多余的代码

--- src/main/java/com/example/SpringBootDemoApplication.java
+++ src/main/java/com/example/SpringBootDemoApplication.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -14,9 +13,4 @@ public class SpringBootDemoApplication extends SpringBootServletInitializer {
         return builder.sources(SpringBootDemoApplication.class);
     }
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(SpringBootDemoApplication.class);
-        app.setBanner(null);
-        app.run(args);
-    }
 }
