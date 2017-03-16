b0b9575e21be5cc258493cda661c07695862f745
likunyang <yanglikun0@gmail.com>
2017-03-16 20:33:28 +0800

    手动指定新版本thymeleaf

--- src/main/java/com/example/SpringBootDemoApplication.java
+++ src/main/java/com/example/SpringBootDemoApplication.java
@@ -2,14 +2,21 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ThymeleafAutoConfiguration.class})
 public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(SpringBootDemoApplication.class);
-		app.setBanner(null);
-		app.run(args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootDemoApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SpringBootDemoApplication.class);
+        app.setBanner(null);
+        app.run(args);
+    }
 }
